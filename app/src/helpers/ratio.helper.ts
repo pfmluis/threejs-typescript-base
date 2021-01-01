2971751af4bc65606a85dccd16728dec618d7dc9
pedroluis <pedro.luis@edirectinsure.com>
2021-01-01 20:24:49 +0000

    Added aspect ratio calculator

---
+++ app/src/helpers/ratio.helper.ts
@@ -0,0 +1,24 @@
+import { 
+  PerspectiveCamera,
+  Renderer,
+} from 'three';
+
+export class PerspectiveCameraRatioHelper {
+
+  public static startResizeListener(camera: PerspectiveCamera, renderer: Renderer): void {
+    window.addEventListener('resize', () => {
+      PerspectiveCameraRatioHelper.resizeRenderer(renderer);
+      PerspectiveCameraRatioHelper.resizeCameraAspect(camera);
+    });
+  }
+
+  private static resizeRenderer(renderer: Renderer): void {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  private static resizeCameraAspect(camera: PerspectiveCamera): void {
+    const aspectRatio: number = window.innerWidth / window.innerHeight;
+    camera.aspect = aspectRatio
+    camera.updateProjectionMatrix();
+  }
+}
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,2 +1,46 @@
+import { 
+  BoxGeometry,
+  Camera,
+  DirectionalLight,
+  Light,
+  Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  Renderer,
+  Scene,
+  WebGLRenderer
+} from 'three';
+import './helpers/ratio.helper';
+import { PerspectiveCameraRatioHelper } from './helpers/ratio.helper';
+
+const scene: Scene = new Scene();
+const camera: Camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 10);
+const renderer: Renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+PerspectiveCameraRatioHelper.startResizeListener(camera as PerspectiveCamera, renderer);
+
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+
+const light: Light = new DirectionalLight(0xffffff, 1);
+light.position.set(0, 3, 2);
+light.lookAt(0, 0, 0);
+scene.add(light);
+
+const cubeGeometry = new BoxGeometry(1, 1, 1);
+const cubeMaterial = new MeshPhongMaterial({ color: 0xff0000 });
+const cube = new Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+
+function animate() {
+  
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate)
+}
+
+animate();
+
+document.body.appendChild(renderer.domElement);
 
 
